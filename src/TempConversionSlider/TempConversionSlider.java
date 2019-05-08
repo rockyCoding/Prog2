0a24781cc889a30f40ead2cae72479218ca81a64
rockyCoding <kuhn897@gmail.com>
2019-05-08 23:26:07 +0200

    Developing TempConversionSlider further, lacking validation circle bindings

--- src/TempConversionSlider/TempConversionSlider.java
+++ src/TempConversionSlider/TempConversionSlider.java
@@ -1,116 +1,58 @@
 package TempConversionSlider;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 
 public class TempConversionSlider extends Application {
 
-    private static double exact, rough;
+    private static SimpleDoubleProperty precise, rough;
     private static Stage window1;
     private static Slider slider;
     private static Label lblTop, sliderTempLabel, celc, cRough, validation;
-    private static VBox mainBox, sliderBox, resultBox, valBox;
-    private static HBox cResBox, cRoughBox;
-
+    private static VBox mainBox, sliderBox, resultBox;
+    private static HBox subBox;
+    private static Circle valCircle;
 
 
     @Override
     public void start(Stage Stage){
         window1 = Stage;
         window1.setTitle("Temperaturkonverter: F => °C");
+        window1.setMinWidth(500);
+        window1.setMaxHeight(320);
 
         Scene mainFrame = createMainframe();
 
-        sliderTempLabel.textProperty().bind(slider.valueProperty().asString("%f F"));
+        sliderTempLabel.textProperty().bind(slider.valueProperty().asString("%.2f F"));
 
         window1.setScene(mainFrame);
         window1.show();
 
-        /*cToF.setOnAction(e -> converter('c', celcIn.getText(), 1.8));
-        fToC.setOnAction(e -> converter('f', fahrIn.getText(), 1.8));
-        fCR.setOnAction(e -> converter('f', fahrIn.getText(), 2));
-        reset.setOnAction(e -> resetMainFrame());*/
+        celc.textProperty().bind(slider.valueProperty().subtract(32).divide(1.8).asString("Genaue Berechnung: %.2f °C"));
+        cRough.textProperty().bind(slider.valueProperty().subtract(32).divide(2).asString("Grobe Berechnung: %.2f °C"));
 
 
     }
-
-    /*static void converter(char inSystem, String inString, double factor){
-        double calc;
-        DecimalFormat df2 = new DecimalFormat("###.##");
-        if ((!restriction)||(counter < 3)) {
-            try {
-                double inValue = Double.parseDouble(inString);
-
-                if (inSystem == 'c') {
-                    validation.setText("");
-                    calc = inValue * factor + 32;
-                    result = new Label(Double.valueOf(df2.format(calc)) + " F");
-                    fieldSwitch(inSystem);
-                    counter++;
-                    counted.setText("Bisher getätigte Umrechnungen: " + counter);
-                } else {
-                    validation.setText("");
-                    calc = (inValue - 32) / factor;
-                    result = new Label(Double.valueOf(df2.format(calc)) + " °C");
-                    fieldSwitch(inSystem);
-                    counter++;
-                    counted.setText("Bisher getätigte Umrechnungen: " + counter);
-                }
-            } catch (Exception e) {
-                validation.setTextFill(Color.web("#ce0a0a"));
-                validation.setText("Eingabe ungültig! \nBitte nur Zahlen eingeben. (Bsp. 23.5)");
-                inString = null;
-                celcIn.clear();
-                fahrIn.clear();
-            }
-        }
-        else {
-            AlertHandler.handleAlert(window1);
-        }
-
-    }*/
-
-    /*static void fieldSwitch(char inSystem){
-        if (inSystem == 'c'){
-            mainBox.getChildren().remove(buttonRow);
-            fFields.getChildren().remove(fahrIn);
-            fFields.getChildren().add(result);
-        }
-        else {
-            mainBox.getChildren().remove(buttonRow);
-            cFields.getChildren().remove(celcIn);
-            cFields.getChildren().add(result);
-        }
-    }
-
-    static void resetMainFrame(){
-        if((!restriction)||(counter < 3)) {
-            window1.setScene(createMainframe());
-        }
-        else{
-            AlertHandler.handleAlert(window1);
-        }
-    }*/
 
     static Scene createMainframe(){
         mainBox = new VBox(10);
         mainBox.setAlignment(Pos.CENTER);
-        mainBox.setSpacing(10);
+        mainBox.setSpacing(30);
         mainBox.setPadding(new Insets(15, 15, 15, 15));
-        mainBox.setStyle("-fx-background-color: lightblue");
+
 
         Scene mainFrame = new Scene(mainBox, 450, 320);
 
@@ -118,44 +60,46 @@ public class TempConversionSlider extends Application {
         lblTop = new Label("Stellen Sie die Temperatur in Fahrenheit mit dem Slider ein.\n"
                 +"Das Programm rechnet die Werte automtisch um.");
 
-        // Creating slider box with label and styles
+        // Setting up slider box with label and styles
         slider = new Slider(-30, 90, 0);
         slider.setMajorTickUnit(1);
         slider.setMinorTickCount(9);
         slider.setOrientation(Orientation.VERTICAL);
-        //slider.setStyle();
         sliderTempLabel = new Label();
+        sliderTempLabel.setStyle("-fx-font-size: 20");
+        sliderTempLabel.setStyle("-fx-font-weight: 700");
         sliderBox = new VBox();
         sliderBox.getChildren().addAll(sliderTempLabel, slider);
         sliderBox.setAlignment(Pos.CENTER);
+        sliderBox.setMinWidth(100);
 
-        // not here
-        /*celc = new Label("Genaue Umrechnung: ");
-        cRough = new Label("Grobe Umrechnung: ");
-        validation = new Label("Verlässlichkeit grobe Umrechnung:");
+        // Setting up the results box with all labels and styles
+        celc = new Label("Genaue Berechnung: -.-- °C");
+        celc.setStyle("-fx-font-size: 20");
+        celc.setStyle("-fx-font-weight: 700");
+        cRough = new Label("Grobe Berechnung: -.-- °C");
+        cRough.setStyle("-fx-font-size: 20");
+        cRough.setStyle("-fx-font-weight: 700");
+        validation = new Label("Verlässlichkeit des groben Resultats:");
+        validation.setStyle("-fx-font-size: 20");
+        validation.setStyle("-fx-font-weight: 700");
+        valCircle = new Circle();
+        valCircle.setRadius(15);
+        valCircle.setFill(Color.LIME);
 
-        cResBox = new HBox();
-        cResBox.getChildren().addAll(celc);
+        resultBox = new VBox();
+        resultBox.getChildren().addAll(celc, cRough, validation, valCircle);
+        resultBox.setMinWidth(250);
+        resultBox.setSpacing(20);
 
-        cRoughBox = new HBox();*/
+        // Sorting alignment of slider box and result box in HBox subBox
+        subBox = new HBox();
+        subBox.getChildren().addAll(sliderBox, resultBox);
+        subBox.setSpacing(20);
 
-
-        //Buildung structure for correct styling and alignment of input fields
-        /*cFields.getChildren().addAll(celc, celcIn);
-        cFields.setAlignment(Pos.CENTER);
-        fFields.getChildren().addAll(cRough, fahrIn);
-        fFields.setAlignment(Pos.CENTER);
-        inFields.getChildren().addAll(cFields, fFields);
-        inFields.setAlignment(Pos.CENTER);*/
-
-
-        // Building row of converter buttons
-        /*buttonRow = new HBox(5);
-        buttonRow.getChildren().addAll(cToF, fToC, fCR);
-        buttonRow.setAlignment(Pos.CENTER);*/
 
         // Building the complete Layout of the main frame
-        mainBox.getChildren().addAll(lblTop, sliderBox);
+        mainBox.getChildren().addAll(lblTop, subBox);
         return mainFrame;
     }
 
